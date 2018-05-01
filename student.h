3b75afa10a0f062c74f3934a123fb5a81256b506
KialRyan <kleiteryan@mail.csuchico.edu>
2018-04-30 17:23:49 -0700

    creates student.h lol

---
+++ student.h
@@ -0,0 +1,16 @@
+#ifndef _STUDENT_H_
+#define _STUDENT_H_
+
+class student
+{
+    private:
+    string firstName;
+    string lastName;
+    public:
+    void setName(string firstName, string lastName);
+    string fullName(string fullName); 
+
+};
+
+
+#endif
